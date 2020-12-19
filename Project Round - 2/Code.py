c391af7aa07a8507b5aae8135a4a33e1281a03bc
Apoorv023 <64470397+Apoorv023@users.noreply.github.com>
2020-12-19 16:16:42 +0530

    Update Code.py

--- Project Round - 2/Code.py
+++ Project Round - 2/Code.py
@@ -1,8 +1,8 @@
-from nltk.corpus import wordnet as wn 	# Import wordnet from the NLTK
-from textblob import TextBlob 			# For pos tagging
-import matplotlib.pyplot as plt 		# For wordcloud as well
+from nltk.corpus import wordnet as wn 				# Import wordnet from the NLTK
+from textblob import TextBlob 					# For pos tagging
+import matplotlib.pyplot as plt 				# For wordcloud as well
 import spacy							# For entity recognition and relationship
-from spacy import displacy				# For visualization of realationships among enities with the help of browser
+from spacy import displacy					# For visualization of realationships among enities with the help of browser
 
 # Opening book - 1 and reading its data in 'T1'
 f1 = open('book1023.txt', 'r', encoding = 'utf-8')
@@ -23,7 +23,6 @@ verb2 = [w for (w, pos) in TextBlob(t2).pos_tags if pos[0] == 'V']
 
 # Creating an empty list that will contain the category of the noun for each word present in the list 'noun1' 
 noun_wordnet1 = []
-
 # Finding the category of noun for each word present in the list 'noun1'
 for x in range(len(noun1)):
 	col = []
@@ -36,7 +35,6 @@ for x in range(len(noun1)):
 
 # Creating an empty list that will contain the category of the verb for each word present in the list 'verb1' 
 verb_wordnet1 = []
-
 # Finding the category of verb for each word present in the list 'verb1'
 for x in range(len(verb1)):
 	col = []
@@ -46,10 +44,9 @@ for x in range(len(verb1)):
 		verb_wordnet1.append(col[0])
 	else:
 		verb_wordnet1.append("Category not found")
-print(verb_wordnet1)
+
 # Creating an empty list that will contain the category of the noun for each word present in the list 'noun2' 
 noun_wordnet2 = []
-
 # Finding the category of noun for each word present in the list 'noun2'
 for x in range(len(noun2)):
 	col = []
@@ -62,7 +59,6 @@ for x in range(len(noun2)):
 
 # Creating an empty list that will contain the category of the verb for each word present in the list 'verb2' 
 verb_wordnet2 = []
-
 # Finding the category of verb for each word present in the list 'verb2'
 for x in range(len(verb2)):
 	col = []
@@ -101,7 +97,7 @@ for i in range(lg2):
 # Recognizing relationships among various entities present in book-1 sentence-wise for each paragraph using 'data1'
 print("\n\t\t\t\t\tEntity Relationship for BOOK-1:")
 for i in range(lg1):
-	dt1 = data1[i].split('.')												# Splitting the paragraph present in 'data1' to create a list(dt1) of sentences
+	dt1 = data1[i].split('.')								# Splitting the paragraph present in 'data1' to create a list(dt1) of sentences
 	for j in range(len(dt1)):
 		piano_doc = sp(dt1[j])
 		print("\n\tParagraph No. -", i+1, ", sentence no. -", j+1)
